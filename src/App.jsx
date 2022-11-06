5e0378d211c804caa1797b6ff03001e909600dae
alekspanteli <alekspanteli@rambler.ru>
2022-11-06 13:38:25 +0000

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 // import reactLogo from './assets/react.svg'
 import './App.css'
+
+
+const Home = () => <h1 className='text-2xl font-bold'>Home</h1>
+const Settings = () => <h1 className='text-2xl font-bold'>Settings</h1>
+const About = () => <h1 className='text-2xl font-bold'>About</h1>
+
 
 function App() {
 
   return (
-    <Router>
-      <div className='xl:container xl:mx-auto px-4'>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-      </div>
-    </Router>
+    <div className='xl:container xl:mx-auto px-4'>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
