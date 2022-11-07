87bf9ea80da8a44c09b70bbf6413fac2a4dd5ff4
alekspanteli <alekspanteli@rambler.ru>
2022-11-07 00:06:33 +0000

    create starter layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 // import reactLogo from './assets/react.svg'
 import './App.css'
 
@@ -11,13 +11,26 @@ const About = () => <h1 className='text-2xl font-bold'>About</h1>
 function App() {
 
   return (
-    <div className='xl:container xl:mx-auto px-4'>
+    <div className='xl:container xl:mx-auto mt-8 px-4'>
+
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+        <header className='flex items-center justify-between'>
+          <Link to='/'>Logo</Link>
+          <nav>
+            <ul className='flex gap-3'>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/settings'>Settings</Link></li>
+              <li><Link to='/about'>About</Link></li>
+            </ul>
+          </nav>
+        </header>
+        <main className='pt-10'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/settings' element={<Settings />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </main>
       </Router>
     </div>
   )
